35767ec2924a33e82ee2c10ebd1c72bd4ed608fb
wellingtonmacena <wellington.msantos@bantec.com.br>
2020-04-03 21:44:57 -0300

    Criação de Backend

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const app = express();
+
+mongoose.connect('mongodb+srv://well:bta456@simples-suffb.azure.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json())
+app.use(routes)
+
+
+
+
+app.listen(3333) 
